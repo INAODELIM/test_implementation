aafbac1f33787d2cd5a5cab1cd873ad0efc2bac5
nigoumiguiala <nigoumiguiala@INAO-n085.interco75.inao>
2018-05-30 18:28:41 +0200

    premiere version interface

---
+++ assets/js/principale.js
@@ -0,0 +1,35 @@
+/* 
+ *created on the 30/05/2018 By GUIALA Jean Roger
+ */
+
+
+
+
+/*$("#ajusteur").click(function () {
+    var columns_container = $(".dynamic-columns");
+    if (!columns_container.hasClass("expanded")) {
+        $(".dynamic-columns .col:first").removeClass("col-lg-9");
+        $(".dynamic-columns .col:first").addClass("col-lg-12");
+
+        $("#ajusteur .aj").removeClass("fa-arrow-right");
+        $("#ajusteur .aj").addClass("fa-arrow-left");
+
+        $(".dynamic-columns .col:last-child").removeClass("col-lg-3");
+        $(".dynamic-columns .col:last-child").addClass("invisible");
+        columns_container.toggleClass("expanded");
+        map.render();
+    } else {
+        $(".dynamic-columns .col:first").removeClass("col-lg-12");
+        $(".dynamic-columns .col:first").addClass("col-lg-9");
+
+        $("#ajusteur .aj").removeClass("fa-arrow-left");
+        $("#ajusteur .aj").addClass("fa-arrow-right");
+
+        $(".dynamic-columns .col:last-child").removeClass("invisible");
+        $(".dynamic-columns .col:last-child").addClass("col-lg-3");
+
+        columns_container.toggleClass("expanded");
+        map.render();
+    }
+});*/
+
